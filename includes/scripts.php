28c8dc8b11d9ebfc386075a397948b7196e975cd
Ivonne Mtz <54880593+Ivonnemtz96@users.noreply.github.com>
2023-05-12 13:30:56 -0700

    Se eliminan datos de plantilla anterior

---
+++ includes/scripts.php
@@ -0,0 +1,13 @@
+<!-- Integrated important scripts here -->
+<script src="js/jquery-3.6.0.min.js"></script>
+<script src="js/jquery.nice-select.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/jquery.appear.min.js"></script>
+<script src="js/wow.min.js"></script>
+<script src="js/owl.carousel.min.js"></script>
+<script src="js/jquery.event.move.js"></script>
+<script src="js/jquery.twentytwenty.js"></script>
+<script src="js/tilt.jquery.min.js"></script>
+<script src="js/magnific-popup.min.js"></script>
+<script src="js/backtotop.js"></script>
+<script src="js/trigger.js"></script>
---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+$title = 'Inicio';
+$inicio = 'active';
+include('includes/head.php');
+?>
+
+<body>
+
+    <?php
+    include('includes/header.php');
+    include('modules/index.php');
+    include('includes/footer.php');
+    include('includes/scripts.php');
+        ?>
+</body>
+
+</html>
