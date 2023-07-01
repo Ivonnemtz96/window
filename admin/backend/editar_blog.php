0aac00e3b666a58a9bd863894fb65bdcef18e6e7
Ivonne Mtz <54880593+Ivonnemtz96@users.noreply.github.com>
2023-06-30 17:05:10 -0700

    No se termina de realizar

--- admin/backend/editar_blog.php
+++ admin/backend/editar_blog.php
@@ -1,12 +1,28 @@
 <?
+// Incluir archivo de conexión a la base de datos
+require_once($_SERVER["DOCUMENT_ROOT"]."/admin/backend/config.php");
+
  session_start();
  if (!isset($_SESSION['user_id'])) {
      header("Location: /admin/login/");
      exit();
  }
 
+ $timezone = new DateTimeZone('America/Mexico_City');
+ $fecha = new DateTime('now', $timezone);
+ $fechaFormateada = $fecha->format('Y-m-d H:i:s');
 
- $blogId= $_GET['blogID']
+ setlocale(LC_ALL, 'es_MX');
+ $mesr = $fecha->format('m');
+ $year = $fecha->format('Y');
 
+
+ $blogId= $_GET['blogID'];
+
+    
+
+    // Obtener los artículos del blog desde la base de datos
+    $query = "SELECT * FROM blog ORDER BY blog_fecha DESC";
+    $resultado = mysqli_query($conn, $query);
 
 ?>
